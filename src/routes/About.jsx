9f535dcd17f9f1ac2392531d55668e5868817ad0
FLucasF <lucasfelipegpGITHUB@gmail.com>
2024-05-16 16:23:00 -0300

    primeira implementação de perguntas

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,17 +1,40 @@
-import React from 'react'
-import './About.css'
-import quizQuestions from '../data/questions.json'
+import React, { useState, useEffect } from 'react'; 
+import './About.css';
+import quizQuestions from '../data/questions.json'; 
 
 const About = () => {
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    setQuestions(quizQuestions);
+  }, []);
+
+  const handleAnswerClick = (answer) => {
+    console.log("Resposta selecionada:", answer); //TODO: Tirar console? tem necessidade?
+    const nextIndex = currentQuestionIndex + 1;
+    if (nextIndex < questions.length) {
+      setCurrentQuestionIndex(nextIndex);
+    } else {
+      alert("Você completou o quiz!"); //TODO: Colocar para que as perguntas e respostas sejam embaralhadas novamente ou quando acabar colocar para ir para tela inicial ou formulário
+      setCurrentQuestionIndex(0);
+    }
+  };
+
   return (
-    <div class="button-container">
-        <button class="custom-button">Botão 1</button>
-        <button class="custom-button">Botão 2</button>
-        <button class="custom-button">Botão 3</button>
-        <button class="custom-button">Botão 4</button>
+    <div className="button-container">
+      {questions.length > 0 && (
+        <>
+          <h3>{questions[currentQuestionIndex].question}</h3>
+          {questions[currentQuestionIndex].answers.map((answer, idx) => (
+            <button key={idx} className="custom-button" onClick={() => handleAnswerClick(answer)}>
+              {answer}
+            </button>
+          ))}
+        </>
+      )}
     </div>
-  )
+  );
 }
 
-export default About
-
+export default About;
