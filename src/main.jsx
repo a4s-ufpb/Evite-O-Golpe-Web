ba039e608dccdc18b0c9c32c854d144df157eb17
FLucasF <lucasfelipegpGITHUB@gmail.com>
2024-06-06 13:57:32 -0300

    terceira tentativa pages

--- src/main.jsx
+++ src/main.jsx
@@ -16,7 +16,7 @@ const router = createBrowserRouter([
     element: <App />,
     children: [
       {
-        path: "/",
+        path: "/EviteOgolpeWEB",
         element: <Home />
       },
       {
