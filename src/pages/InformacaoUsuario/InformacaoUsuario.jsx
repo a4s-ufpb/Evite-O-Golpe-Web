2e5badc5b82f30903aa1a412b5573c0bde602277
FLucasF <lucasfelipegpGITHUB@gmail.com>
2024-06-21 13:23:41 -0300

    atualizando imagem do home para mais qualidade e sem os números

--- src/pages/InformacaoUsuario/InformacaoUsuario.jsx
+++ src/pages/InformacaoUsuario/InformacaoUsuario.jsx
@@ -1,29 +1,29 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import styles from './InformacaoUsuario.module.css'; // Ensure correct path
+import styles from './InformacaoUsuario.module.css'; // Certifique-se de que o caminho está correto
 
 const InformacaoUsuario = () => {
   const [userID, setUserID] = useState('');
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-    if (userID.trim()) {
-      sessionStorage.setItem('userID', userID);
-      navigate('/EviteOgolpeWEB/Quiz');
+    e.preventDefault();  // Impede o comportamento padrão do formulário
+    if (userID.trim()) {  // Checa se o userID não é apenas espaços em branco
+      sessionStorage.setItem('userID', userID);  // Salva o userID no sessionStorage
+      navigate('/EviteOgolpeWEB/Quiz');  // Navega para a página do quiz
     }
   };
 
   const handleAnonymousSubmit = () => {
-    sessionStorage.setItem('userID', 'Anonymous');
-    navigate('/EviteOgolpeWEB/Quiz');
+    sessionStorage.setItem('userID', 'Anonymous');  // Define o userID como 'Anonymous'
+    navigate('/EviteOgolpeWEB/Quiz');  // Navega para a página do quiz
   };
 
   return (
     <div className={styles.container}>
       <div className={styles.content}>
         <h2>Digite seu nome:</h2>
-        <form onSubmit={handleSubmit}>
+        <form id="userForm" onSubmit={handleSubmit}>
           <input
             type="text"
             id="userID"
@@ -34,13 +34,12 @@ const InformacaoUsuario = () => {
           />
         </form>
         <div className={styles.buttonContainer}>
-        <button type="submit" form="userForm">Enviar</button>
+            <button type="submit" form="userForm">Enviar</button>
+        </div>
+        <p>ou</p>
+        <button onClick={handleAnonymousSubmit} className={styles.noStyleLink}>Entrar sem meus dados</button>
       </div>
-      <p>ou</p>
-      <button onClick={handleAnonymousSubmit} className={styles.noStyleLink}>Entrar sem meus dados</button>
     </div>
-  </div>
-
   );
 }
 
