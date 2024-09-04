a9fe144c6b3a7ee8f58e76118245b97ae740a928
FLucasF <lucasfelipegpGITHUB@gmail.com>
2024-09-04 09:26:40 -0300

    ajeitando problema na navbar, não estava selecionando a palavra referente a rota

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -28,19 +28,19 @@ function Navbar() {
 
       <ul className={clicked ? 'navbar active' : 'navbar'}>
         <li>
-          <Link to="/" className={isActive("/EviteOgolpeWEB/") ? "active" : "notActive"} onClick={closeMenu}>Home</Link>
+          <Link to="/" className={isActive("/") ? "active" : ""} onClick={closeMenu}>Home</Link>
         </li>
         <li>
-          <Link to="/InformacaoUsuario" className={isActive("/EviteOgolpeWEB/InformacaoUsuario") ? "active" : ""} onClick={closeMenu}>Quiz</Link>
+          <Link to="/InformacaoUsuario" className={isActive("/InformacaoUsuario") ? "active" : ""} onClick={closeMenu}>Quiz</Link>
         </li>
         <li>
-          <Link to="/Tutorial" className={isActive("/EviteOgolpeWEB/Tutorial") ? "active" : ""} onClick={closeMenu}>Tutorial</Link>
+          <Link to="/Tutorial" className={isActive("/Tutorial") ? "active" : ""} onClick={closeMenu}>Tutorial</Link>
         </li>
         <li>
-        <Link to="/Informacoes" className={isActive("/EviteOgolpeWEB/Informacoes") ? "active" : "inactive"} onClick={closeMenu}>Mais Informações</Link>
+          <Link to="/Informacoes" className={isActive("/Informacoes") ? "active" : ""} onClick={closeMenu}>Mais Informações</Link>
         </li>
         <li>
-          <Link to="/Sobre" className={isActive("/EviteOgolpeWEB/Sobre") ? "active" : "notActive"} onClick={closeMenu}>Sobre</Link>
+          <Link to="/Sobre" className={isActive("/Sobre") ? "active" : ""} onClick={closeMenu}>Sobre</Link>
         </li>
         <li>
           <a href="https://bit.ly/form_evite_o_golpe" target="_blank" rel="noopener noreferrer" onClick={closeMenu}>Feedback</a>
